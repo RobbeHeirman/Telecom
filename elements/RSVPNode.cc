855f1a348be2f0c348d4117f35e855b65cbd2276
robbeheirman <robbeheirman@msn.com>
2018-12-27 21:05:23 +0100

    Set up the loop to dissect the Path message

--- elements/RSVPNode.cc
+++ elements/RSVPNode.cc
@@ -6,9 +6,25 @@ CLICK_DECLS
 
 void RSVPNode::push(int port, Packet* p){
 
-    RSVPHeader* start_header = (RSVPHeader*) p->data();
-    if(start_header->msg_type == 1){
-        click_chatter("yaaay");
+    // We know a RSVP message start with the RSVP header.
+    // We can cast directly.
+    RSVPHeader* header = (RSVPHeader*) p->data();
+
+
+
+    // If we receive a PATH message
+    if(header->msg_type == RSVPHeader::Type::Path){
+        RSVPObject* session = (RSVPObject*) (header + 1 ) ;
+
+        while((const unsigned  char*)session < p->end_data()){
+
+            if(session->class_num == RSVPObject::Class::Session){
+                click_chatter("YAY");
+            }
+            session = (RSVPObject*) (session + 1);
+        }
+    click_chatter("I have reached the end");
+
     }
     output(port).push(p);
 }
