369bd4dda809412dcda50f5b503582e2aca29828
robbeheirman <robbeheirman@msn.com>
2018-12-27 15:48:26 +0100

    INIT of a RVSPNODE

---
+++ elements/RSVPNode.cc
@@ -0,0 +1,10 @@
+//
+// Created by robbe on 27/12/18.
+//
+
+#include "RSVPNode.hh"
+
+CLICK_DECLS
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(RSVPNODE)
---
+++ elements/RSVPNode.hh
@@ -0,0 +1,24 @@
+/**
+ * @authors: Robbe Heirman, Jules Desmet
+ * @brief: Class representing a click node element that will handle path messages.
+**/
+
+#ifndef TELECOM_RSVPNODE_H
+#define TELECOM_RSVPNODE_H
+
+#include <click/element.hh>
+
+
+CLICK_DECLS
+
+class RSVPNode: public Element {
+
+    RSVPHost() = default;
+    ~RSVPHost() = default;
+
+
+};
+
+CLICK_ENDDECLS
+
+#endif //TELECOM_RSVPNODEPATH_H
